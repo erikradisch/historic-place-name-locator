e6227bef2955d9946f898ff1e1043d9eb49474e7
erikradisch <34335811+erikradisch@users.noreply.github.com>
2019-09-05 11:33:35 +0200

    Update find_places_example.py

--- find_places_example.py
+++ find_places_example.py
@@ -10,9 +10,9 @@ pd.set_option('expand_frame_repr', False)
 print(datei)
 dfplaces = pd.read_csv(datei,  header=0, index_col=0 , sep=',', decimal=',')
 
-regions = gpd.read_file('deutschesreich/pfarreien.geojson')
+regions = gpd.read_file('GERMANEMPIRE.shp')
 
 dfplaces['code']=np.nan
-geo_region = gpd.read_file('deutschesreich/pfarreien.geojson')
+geo_region = gpd.read_file('GERMANEMPIRE.shp')
 
 placelocator(dfplaces, regions,geo_region, 'config.json')
