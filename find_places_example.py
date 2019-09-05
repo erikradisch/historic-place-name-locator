34f30b6fd9b866a2e9f9fdada612115e20542ac0
erikradisch <34335811+erikradisch@users.noreply.github.com>
2019-09-05 11:35:34 +0200

    Update find_places_example.py

--- find_places_example.py
+++ find_places_example.py
@@ -2,17 +2,13 @@ from findplaces import placelocator
 import pandas as pd
 import geopandas as gpd
 import numpy as np
-#datei = 'Orte-Register_bereinigt2.csv'
-datei = '2018_Gold_GenDB_all2.csv'
+datei = 'place_sample.csv'
 pd.options.display.max_columns = 999
 pd.set_option('expand_frame_repr', False)
 
 print(datei)
-dfplaces = pd.read_csv(datei,  header=0, index_col=0 , sep=',', decimal=',')
-
+dfplaces = pd.read_csv(datei,  header=0, index_col=0 , sep='\t', decimal=',')
 regions = gpd.read_file('GERMANEMPIRE.shp')
-
-dfplaces['code']=np.nan
 geo_region = gpd.read_file('GERMANEMPIRE.shp')
 
 placelocator(dfplaces, regions,geo_region, 'config.json')
